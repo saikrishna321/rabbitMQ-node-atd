cd9a0b7ca94d9e28707fd10d8fa79239b1083db1
saikrishna321 <saikrishna321@yahoo.com>
2018-05-30 12:24:14 +0100

    parse json

--- api/routes/devices.js
+++ api/routes/devices.js
@@ -1,29 +1,29 @@
-import {
-    Router
-} from "express";
+import { Router } from "express";
 const router = Router();
 import amqp from "amqplib";
 router.get("/", async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    let a = await getDevices();
-    console.log(a)
-    res.send(a);
-})
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.send(await getDevices());
+});
 
 async function getDevices() {
-    let a;
-    let connection = await amqp.connect('amqp://localhost')
-    let channel = await connection.createChannel();
-    await channel.assertQueue('device', {
-        durable: false
-    }, {
-        noAck: false
-    });
-    let b = await channel.consume('device', (msg) => {
-        a = msg.content.toString();
-        console.log("Inside", a)
-    })
-    return a;
+  let data = [];
+  let connection = await amqp.connect("amqp://localhost");
+  let channel = await connection.createChannel();
+  await channel.assertQueue(
+    "device",
+    {
+      durable: false
+    },
+    {
+      noAck: true
+    }
+  );
+  let b = await channel.consume("device", msg => {
+    let a = msg.content.toString().replace(/(^|[^\\])(\\\\)*\\$/, "$&\\");
+    data.push(JSON.parse(a));
+  });
+  return JSON.stringify(data, null, "\t");
 }
 
 export default router;
